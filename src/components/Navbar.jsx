85f7e527b027bf0096c0938bed0ced17090d8d68
unknown <mo.mahrous80@gmail.com>
2024-08-04 11:26:17 +0300

    fix: navbar position and visual

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,37 +18,39 @@ function NavBar() {
     };
 
     return (
-        <div className="navbar bg-base-100 container mx-auto fixed z-50">
-            <div className="navbar-start">
-                <Link
-                    to="/"
-                    className="btn btn-ghost hover:bg-transparent text-xl"
-                >
-                    <img
-                        src="/blog_icon.ico"
-                        className="mr-3 h-6 sm:h-9"
-                        alt="Flowbite React Logo"
-                    />
-                    Blogly
-                </Link>
-            </div>
+        <div className="bg-base-200  left-0 right-0 fixed z-50 w-full shadow-md">
+            <div className="navbar container  mx-auto ">
+                <div className="navbar-start">
+                    <Link
+                        to="/"
+                        className="btn btn-ghost hover:bg-transparent text-xl"
+                    >
+                        <img
+                            src="/blog_icon.ico"
+                            className="mr-3 h-6 sm:h-9"
+                            alt="Flowbite React Logo"
+                        />
+                        Blogly
+                    </Link>
+                </div>
 
-            <div className="navbar-end">
-                {/* Open the modal using document.getElementById('ID').showModal() method */}
-                <button
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                    onClick={
-                        currentUser
-                            ? handleLogOut
-                            : () =>
-                                  document
-                                      .getElementById("my_modal_1")
-                                      .showModal()
-                    }
-                >
-                    {!currentUser ? "Log in / Sign up" : "Log out"}
-                </button>
-                <Login />
+                <div className="navbar-end">
+                    {/* Open the modal using document.getElementById('ID').showModal() method */}
+                    <button
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        onClick={
+                            currentUser
+                                ? handleLogOut
+                                : () =>
+                                      document
+                                          .getElementById("my_modal_1")
+                                          .showModal()
+                        }
+                    >
+                        {!currentUser ? "Log in / Sign up" : "Log out"}
+                    </button>
+                    <Login />
+                </div>
             </div>
         </div>
     );
